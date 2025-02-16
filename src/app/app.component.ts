55bbb7f5df26e5e436a4536ef98f7750a73c217c
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-02-16 09:30:04 +0530

    components updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  template : 'Welcome to Angular!!',
+  styles: [
+    `
+    :host {
+      color: #a133ed;
+    }
+    `
+  ],
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontEnd';
