3ca21e6146c320aca67770fe04cebe4919799ab0
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-02-16 10:31:36 +0530

    Control flow in componens - @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,31 +49,51 @@ import { RouterOutlet } from '@angular/router';
 
 
 //Control flow in componens - @if
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <div>
+//       @if (isServerRunning) {
+//         <span class="running">😍 Yes, server is running</span>
+//       } @else {
+//         <span class="not-running">😕 No, server is not running</span>
+//       }
+//     </div>
+//   `,
+//   //applying colors based on true false
+//   styles: [
+//     `
+//     .running {
+//       color: green;
+//       font-weight: bold;
+//     }
+//     .not-running {
+//       color: red;
+//       font-weight: bold;
+//     }
+//     `
+//   ],
+// })
+// export class AppComponent {
+//   isServerRunning = false; 
+// }
+
+//Control flow in componens - @for
 @Component({
   selector: 'app-root',
   template: `
-    <div>
-      @if (isServerRunning) {
-        <span class="running">😍 Yes, server is running</span>
-      } @else {
-        <span class="not-running">😕 No, server is not running</span>
-      }
-    </div>
+    @for(user of users; track user.id) {
+      <p>{{ user.name }} is {{ user.age }}</p>
+    }
   `,
-  //applying colors based on true false
-  styles: [
-    `
-    .running {
-      color: green;
-      font-weight: bold;
-    }
-    .not-running {
-      color: red;
-      font-weight: bold;
-    }
-    `
-  ],
 })
+
 export class AppComponent {
-  isServerRunning = false; 
+  users = [
+    {id: 0, name: 'Sarah', age: 23},
+    {id: 1, name: 'John', age: 18},
+    {id: 2, name: 'Willy', age: 20},
+    {id: 3, name: 'Smith', age: 12},
+    {id: 4, name: 'Jane', age: 15},
+  ]
 }
